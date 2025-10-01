67158d659ebda4d3fdac07cdc32f0fc8c1b1133f
KorobovS <korobovs33@yandex.ru>
2025-10-02 00:38:28 +0300

    add edge chrome

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,12 +1,16 @@
 package utils;
 
 import io.qameta.allure.Allure;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
@@ -26,7 +30,7 @@ public abstract class BaseTest {
     }
 
     private void startDriver() {
-        driver = ProjectUtils.createDriver();
+
     }
 
     private void closeDriver() {
@@ -37,10 +41,25 @@ public abstract class BaseTest {
         }
     }
 
+    @Parameters("browser")
     @BeforeMethod
-    protected void beforeMethod(Method method) {
-        startDriver();
-        getDriver().get(config.getBaseUrl());
+    protected void beforeMethod(Method method, String browser) {
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().setSize(new Dimension(1440, 1080));
+        System.out.println(String.format("Открылса браузер: %s", browser));
+
+        driver.get(config.getBaseUrl());
     }
 
     @AfterMethod
--- src/test/java/utils/ProjectUtils.java
+++ src/test/java/utils/ProjectUtils.java
@@ -1,19 +1,30 @@
 package utils;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 
 public class ProjectUtils {
 
-    public static WebDriver createDriver() {
+    public static WebDriver createDriver(String browser) {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
         WebDriver driver;
 
-        chromeOptions.addArguments("--window-size=1440, 1080");
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
 
-        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().setSize(new Dimension(1440, 1080));
 
         return driver;
     }
