6a300995abfe0851d3a5d211c5457546248246dd
KorobovS <korobovs33@yandex.ru>
2025-10-04 19:20:35 +0300

    add ci

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,13 +1,11 @@
 package utils;
 
 import io.qameta.allure.Allure;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +14,10 @@ import org.testng.annotations.Parameters;
 
 import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 import java.util.Objects;
 
 public abstract class BaseTest {
@@ -53,22 +54,44 @@ public abstract class BaseTest {
     @BeforeMethod
     protected void beforeMethod(Method method, @Optional("yandex") String browser) {
 
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                driver = new EdgeDriver();
-                break;
-            case "yandex":
-                System.setProperty("webdriver.chrome.driver", "driver/yandexdriver-25.8.0.1872-win64/yandexdriver.exe");
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--disable-notifications");
-                options.addArguments("--remote-allow-origins=*");
-                driver = new ChromeDriver(options);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+
+        if (remoteUrl != null) {
+            LoggerUtil.info(String.format("SELENIUM_REMOTE_URL = %s", remoteUrl));
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+            Allure.addAttachment("RemoteUrl", remoteUrl);
+            chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--no-sandbox");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.addArguments("--window-size=1920,1080");
+            chromeOptions.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), chromeOptions);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            LoggerUtil.info("Local run");
+
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                case "yandex":
+                    System.setProperty("webdriver.chrome.driver", "driver/yandexdriver-25.8.0.1872-win64/yandexdriver.exe");
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--disable-notifications");
+                    options.addArguments("--remote-allow-origins=*");
+                    driver = new ChromeDriver(options);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported browser: " + browser);
+            }
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
