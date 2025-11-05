07603a4f0825fec2447940f4e2f033297e2fb2f3
KorobovS <korobovs33@yandex.ru>
2025-11-05 07:42:06 +0300

    smoke

--- src/test/java/tests/SchemaMeasurementDataTest.java
+++ src/test/java/tests/SchemaMeasurementDataTest.java
@@ -7,6 +7,8 @@ import org.testng.annotations.Test;
 import pages.LoginPage;
 import utils.BaseTest;
 
+import java.util.List;
+
 import static utils.Data.Dashboard.listParameters;
 
 @Epic("Модуль Схема")
@@ -15,14 +17,14 @@ public class SchemaMeasurementDataTest extends BaseTest {
 
     @Test(groups = "smoke")
     @Tag("smoke")
-    @Description("Работа вкладки Данные измерений модуля Схема, вид отображения - Тренд")
+    @Description("Работа вкладки Данные измерений модуля Схема, вид отображения - Тренд. Выбран один параметр Оборотные")
     @Severity(SeverityLevel.CRITICAL)
     @Link("https://team-b9fb.testit.software/projects/1/tests/66")
-    public void testDisplayTypeTrend() {
+    public void testDisplayTypeTrend1Parameter() {
 
         int count = 1;
 
-        String nameGraph = new LoginPage(getDriver())
+        List<String> nameGraph = new LoginPage(getDriver())
                 .loginToApp()
                 .selectTimeInterval(1, 1, 2020, 23, 7, 10, 2025, 00)
                 .goTo()
@@ -31,8 +33,32 @@ public class SchemaMeasurementDataTest extends BaseTest {
 
         for (String nameParameter : listParameters) {
             Allure.step("Проверяю, что название выбранного параметра соответствует показываемому");
-            Assert.assertEquals(nameGraph, nameParameter);
+            Assert.assertEquals(nameGraph.get(0), nameParameter);
         }
-        Assert.assertEquals(listParameters.size(), count);
+        Assert.assertEquals(nameGraph.size(), count);
+    }
+
+    @Test(groups = "smoke")
+    @Tag("smoke")
+    @Description("Работа вкладки Данные измерений модуля Схема, вид отображения - Тренд. Выбран 5 параметров Замеры из конца списка")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link("https://team-b9fb.testit.software/projects/1/tests/66")
+    public void testDisplayTypeTrend5Parameter() {
+
+        int count = 5;
+
+        List<String> nameGraph = new LoginPage(getDriver())
+                .loginToApp()
+                .selectTimeInterval(1, 1, 2020, 23, 7, 10, 2025, 00)
+                .goTo()
+                .getMeasurementDataGraph("4.2-2G28", count)
+                .getNameGraph();
+
+        Assert.assertEquals(nameGraph.size(), count);
+        for (int i = 0; i < count; i++) {
+            Allure.step("Проверяю, что название выбранного параметра соответствует показываемому");
+            Assert.assertTrue(nameGraph.get(i).contains(listParameters.get(i).split(" ")[0])
+                    || nameGraph.get(i).contains(listParameters.get(i).split(" ")[1]));
+        }
     }
 }
