ee9f8e8659114d09b3e6941d4eccb0778ed13766
KorobovS <korobovs33@yandex.ru>
2025-11-11 21:03:44 +0300

    update getNameGraph

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -543,7 +543,8 @@ public class DashboardPage extends BasePage {
     public List<String> getNameGraph() {
 
         List<String> listNameGraph = new ArrayList<>();
-        getWait5().until(ExpectedConditions.visibilityOf(nameGraph.get(0)));
+//        getWait5().until(ExpectedConditions.visibilityOf(nameGraph.get(0)));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nameGraph.get(0));
 
         for (int i = 0; i < nameGraph.size(); i++) {
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nameGraph.get(i));
